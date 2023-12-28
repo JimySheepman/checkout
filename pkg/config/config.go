455e4f3202ea53df6fc4680fb04c8d8b52a7bdf2
JimySheepman <y.alikoyuncu@gmail.com>
2023-12-28 17:44:07 +0300

    apply hexagonal architecture

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,21 +1,72 @@
 package config
 
-// Configurations exported
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var Cfg Configurations
+
 type Configurations struct {
-	Server       ServerConfigurations
-	Database     DatabaseConfigurations
-	EXAMPLE_PATH string
-	EXAMPLE_VAR  string
+	Server       Server
+	MongoDB      MongoDB
+	LoggerConfig LoggerConfig
 }
 
-// ServerConfigurations exported
-type ServerConfigurations struct {
-	Port int
+type Server struct {
+	RestServer RestServer
+	FileServer FileServer
 }
 
-// DatabaseConfigurations exported
-type DatabaseConfigurations struct {
-	DBName     string
-	DBUser     string
-	DBPassword string
+type RestServer struct {
+	Addr        string
+	PprofEnable int
+}
+
+type FileServer struct {
+	InputPath  string
+	OutputPath string
+}
+
+type LoggerConfig struct {
+	AppName         string
+	LogLevel        int
+	LogEncoding     string
+	GraylogAddr     string
+	EnvironmentType string
+}
+
+type MongoDB struct {
+	Addr       string
+	Port       int
+	User       string
+	Password   string `json:"-"`
+	Timeout    int
+	Name       string
+	Collection string
+}
+
+func LoadConfig() error {
+	// Set the file name of the configurations file
+	viper.SetConfigName("config")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath(".")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+		return err
+	}
+
+	if err := viper.Unmarshal(&Cfg); err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+		return err
+	}
+
+	return nil
 }
