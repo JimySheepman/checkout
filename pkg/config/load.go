9a3c8f598c4493f88da4bdabf47adf473ae10077
JimySheepman <y.alikoyuncu@gmail.com>
2024-01-02 11:01:13 +0300

    apply hexagonal architecture

---
+++ pkg/config/load.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var Cfg Configurations
+
+func LoadConfig() error {
+	// Set the file name of the configurations file
+	viper.SetConfigName("config")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath(".")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+		return err
+	}
+
+	if err := viper.Unmarshal(&Cfg); err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+		return err
+	}
+
+	return nil
+}
